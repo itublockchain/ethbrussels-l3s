5c93d95fcff37ee72f401d7e222ec7bd8e4e7306
Feyyaz Numan Cavlak <feyyazcavlak@outlook.com>
2024-07-12 21:44:41 +0200

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+// Importing Components
+import { Button } from "@/components/ui/button"
+
+function App() {
+
+  return (
+    <>
+      <Button onClick={() => alert("Selam")}>Button</Button>
+    </>
+  )
+}
+
+export default App
